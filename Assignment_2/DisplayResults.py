03e3083143baf9cb2b3ff14b47034676eb44abb2
unknown <s233235@dtu.dk>
2024-03-10 17:45:13 +0100

    Update DisplayResults.py

--- Assignment_2/DisplayResults.py
+++ Assignment_2/DisplayResults.py
@@ -17,20 +17,23 @@ def DisplayResults_and_loading(V,lnd):
     Vm = np.round(np.absolute(V),3) # Voltage magnitude
     Theta = np.round(np.angle(V)*180/np.pi,2) #Voltage angle
 
-    S_inj = V*(Ybus.dot(V)).conj() # Generation and load power
-    Sm = np.round(np.absolute(S_inj),2)
+    S_inj = np.round(V*(Ybus.dot(V)).conj(),3) # Generation and load power
+    # Sm = np.round(np.absolute(S_inj),2)
 
     P_inj_g = np.array([]) #Generation active Power
     P_inj_l = np.array([]) #Load active Power
     Q_inj_g = np.array([]) #Generation reactive Power
     Q_inj_l = np.array([]) #Load reactive Power
     loading_inj = np.array([]) #Loading of generator at bus
+    S_gen = S_inj+SLD
+    S_gen_m = np.round(np.absolute(S_gen),2)
+    print(S_inj, S_gen, SLD)
 
-    for index,s in enumerate(S_inj):
-        if np.real(s)>=0: 
+    for index,s in enumerate(S_gen):
+        if np.real(s)>0: 
             P_inj_g = np.concatenate((P_inj_g, [str(np.round(np.real(s),3))]))
             Q_inj_g = np.concatenate((Q_inj_g, [str(np.round(np.imag(s),3))]))
-            loading_inj = np.concatenate((loading_inj,["{0}%".format(str(np.round(s*100/Gen_MVA[index],2)))]))
+            loading_inj = np.concatenate((loading_inj,["{0}%".format(str(np.round(S_gen_m[index]*100/Gen_MVA[index],2)))]))
         else:
             P_inj_g = np.concatenate((P_inj_g, ["-"]))
             Q_inj_g = np.concatenate((Q_inj_g, ["-"]))
@@ -38,29 +41,33 @@ def DisplayResults_and_loading(V,lnd):
 
         if SLD[index] != 0:
             P_inj_l = np.concatenate((P_inj_l, [str(np.round(np.real(SLD[index]),3))]))
-            Q_inj_l = np.concatenate((Q_inj_l, [str(np.round(-np.imag(SLD[index]),3))]))
+            Q_inj_l = np.concatenate((Q_inj_l, [str(np.round(np.imag(SLD[index]),3))]))
         else:
-            P_inj_l = np.concatenate((P_inj_g, ["-"]))
-            Q_inj_l = np.concatenate((Q_inj_g, ["-"]))
+            P_inj_l = np.concatenate((P_inj_l, ["-"]))
+            Q_inj_l = np.concatenate((Q_inj_l, ["-"]))
         
 
     # Branches
     n_br = len(br_f) # Number of Branches
     branch_no = np.arange(1,(n_br+1)) # Branch numbers
 
+    loading_from = np.array([])
+    loading_to = np.array([])
     S_to = V[br_t]*Y_to.dot(V).conj() # Apparent Power flowing into the receiving end busses
-    loading_to= np.round(S_to*100/br_MVA,3) # Loading of line at to branch
+    S_to_m = np.absolute(S_to)
+    for index,s in enumerate(S_to) :
+        loading_to = np.concatenate((loading_to,["{0}%".format(str(np.round(S_to_m[index]*100/br_MVA[index],2)))])) # Loading of line at to branch
     S_from = V[br_f]*Y_from.dot(V).conj() # Apparent Power flowing from the receiving end busses
-    loading_from= np.round(S_to*100/br_MVA,3) # Loading of line at from branch
+    S_from_m = np.absolute(S_from)
+    for index,s in enumerate(S_from) :
+        loading_from = np.concatenate((loading_from,["{0}%".format(str(np.round(S_from_m[index]*100/br_MVA[index],2)))])) # Loading of line at from branch
     # Having number of branches and not there index
     mapping_function = np.vectorize(lambda x: ind_to_bus[x])
     br_from = mapping_function(br_f)
     br_to = mapping_function(br_t)
 
-   
 
-
-    bus_data = np.vstack((bus_no, bus_labels, Vm, Theta, Sm, P_inj_g, Q_inj_g, loading_inj, P_inj_l, Q_inj_l))
+    bus_data = np.vstack((bus_no, bus_labels, Vm, Theta, P_inj_g, Q_inj_g, loading_inj, P_inj_l, Q_inj_l))
     bus_data = bus_data.transpose()
 
 
@@ -72,13 +79,14 @@ def DisplayResults_and_loading(V,lnd):
     print("================================================================================")
     print("                           | Bus results |                             ")
     print("================================================================================")
-    print("Bus    Bus            Voltage          Power     Generation          Load   ")
+    print("Bus    Bus            Voltage                Generation                Load   ")
 
-    print(tabulate(bus_data, headers=["#", "Label", "Mag(pu)", "Ang(deg)", "S(pu)","P(pu)","Q(pu)","loading","P(pu)","Q(pu)"],stralign="center"))
+    print(tabulate(bus_data, headers=["#", "Label", "Mag(pu)", "Ang(deg)","P(pu)","Q(pu)","loading","P(pu)","Q(pu)"],stralign="center"))
     print("\n")
     # Display Branch flow
     print("=============================================================")
     print("                      | Branch flow |                        ")
     print("=============================================================")
-    print("Branch  From    To       From Bus Inject.      To Bus Inject.  ")
+    print("Branch  From    To         From Bus Inject.         To Bus Inject.  ")
     print(tabulate(branch_data, headers=[" # ", "Bus", "Bus", "P(pu)", "Q(pu)", "loading", "P(pu)", "Q(pu)", "loading"],stralign="center"))
+
