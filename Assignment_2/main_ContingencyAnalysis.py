872fa02780bd739354dc6d128b83ea0fdd577915
unknown <s233235@dtu.dk>
2024-03-10 17:45:09 +0100

    Update main_ContingencyAnalysis.py

--- Assignment_2/main_ContingencyAnalysis.py
+++ Assignment_2/main_ContingencyAnalysis.py
@@ -13,14 +13,14 @@ err_tol = 0.0001
 
 # Load the Network data...
 filename = {'Test' : 'TestSystem4SA.txt', 'Nordic' : 'Nordic32_SA.txt'}
-lnd.LoadNetworkData(filename['Test']) # makes Ybus available as lnd.Ybus etc.
+lnd.LoadNetworkData(filename['Nordic']) # makes Ybus available as lnd.Ybus etc.
 
 #%%
 ###################################################################
 # Part I: Study the base case and display results (with % loading)#
 ###################################################################
 
-V,success,n = pf.PowerFlowNewton(lnd.Ybus,lnd.Sbus,lnd.V0,lnd.pv_index,lnd.pq_index,max_iter,err_tol)
+V,success,n = pf.PowerFlowNewton(lnd.Ybus,lnd.Sbus,lnd.V0,lnd.pv_index,lnd.pq_index,max_iter,err_tol,1)
 
 if success: # Display results if the power flow analysis converged
     pf.DisplayResults_and_loading(V,lnd)
@@ -29,46 +29,46 @@ if success: # Display results if the power flow analysis converged
 ######################################################################
 # Part II: Simplifiedcontingencyanalysis(onlybranchoutages) #
 ######################################################################
-
-print('*'*50)
-print('* Contingency Analysis *')
-print('*'*50)
-
-for i in range(len(lnd.br_f)): # sweep over branches
-    fr_ind = lnd.br_f[i]
-    to_ind = lnd.br_t[i]
-    br_ind = i
-    Ybr_mat = lnd.br_Ymat[i]
-    Ybus_mod, Yfr_mod, Yto_mod = pf.apply_contingency_to_Y_matrices(lnd.Ybus,lnd.Y_fr,lnd.Y_to,\
-                                                                 fr_ind,to_ind,br_ind,Ybr_mat)
     
-    str_status = '-'*63 + '\nTripping of branch {:} (bus {:} - bus {:})'.format(i+1, lnd.ind_to_bus[fr_ind],\
-                                                                                lnd.ind_to_bus[to_ind])
+    print('*'*50)
+    print('* Contingency Analysis *')
+    print('*'*50)
     
-    try: # try the load flow, if it fails, display message
-        V,success,n = pf.PowerFlowNewton(Ybus_mod,lnd.Sbus,lnd.V0,lnd.pv_index,lnd.pq_index,\
-                                         max_iter,err_tol,print_progress=False)
+    for i in range(len(lnd.br_f)): # sweep over branches
+        fr_ind = lnd.br_f[i]
+        to_ind = lnd.br_t[i]
+        br_ind = i
+        Ybr_mat = lnd.br_Ymat[i]
+        Ybus_mod, Yfr_mod, Yto_mod = pf.apply_contingency_to_Y_matrices(lnd.Ybus,lnd.Y_fr,lnd.Y_to,\
+                                                                     fr_ind,to_ind,br_ind,Ybr_mat)
         
-    except:
-        str_ = '--> Load Flow error (Jacobian) when branch {:} (bus {:} - bus {:}) is tripped'
-        str_ = str_.format(i+1, lnd.ind_to_bus[fr_ind], lnd.ind_to_bus[to_ind])
-        print(str_status + ' [CONVERGENCE ISSUES!]')
-        print(str_)
-    
-    else:
-        if success: # Display results if the power flow analysis converged
-            violations = pf.System_violations(V,Ybus_mod,Yfr_mod,Yto_mod,lnd)
+        str_status = '-'*63 + '\nTripping of branch {:} (bus {:} - bus {:})'.format(i+1, lnd.ind_to_bus[fr_ind],\
+                                                                                    lnd.ind_to_bus[to_ind])
         
-            if not violations: # no violations, printstatusandmoveon
-                print(str_status + '[OK!]')
+        try: # try the load flow, if it fails, display message
+            V,success,n = pf.PowerFlowNewton(Ybus_mod,lnd.Sbus,lnd.V0,lnd.pv_index,lnd.pq_index,\
+                                             max_iter,err_tol,0)
             
-            else: # ifviolation, displaythem
-                print(str_status + '[Violations!]')
-                for str_ in violations:
-                    print(str_)
-                    
-        else: # no convergence...
-            str_ = '--> No load-flow convergence when branch {:} (bus {:} - bus {:}) is tripped'
+        except:
+            str_ = '--> Load Flow error (Jacobian) when branch {:} (bus {:} - bus {:}) is tripped'
             str_ = str_.format(i+1, lnd.ind_to_bus[fr_ind], lnd.ind_to_bus[to_ind])
             print(str_status + ' [CONVERGENCE ISSUES!]')
             print(str_)
+        
+        else:
+            if success: # Display results if the power flow analysis converged
+                violations = pf.System_violations(V,Ybus_mod,Yfr_mod,Yto_mod,lnd)
+            
+                if not violations: # no violations, printstatusandmoveon
+                    print(str_status + '[OK!]')
+                
+                else: # ifviolation, displaythem
+                    print(str_status + '[Violations!]')
+                    for str_ in violations:
+                        print(str_)
+                        
+            else: # no convergence...
+                str_ = '--> No load-flow convergence when branch {:} (bus {:} - bus {:}) is tripped'
+                str_ = str_.format(i+1, lnd.ind_to_bus[fr_ind], lnd.ind_to_bus[to_ind])
+                print(str_status + ' [CONVERGENCE ISSUES!]')
+                print(str_)
